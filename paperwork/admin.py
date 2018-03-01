af619eb953daab3780d7d932d96c3cbdf0051235
Mark Miller <725mrm@gmail.com>
2018-03-01 04:49:53 -0800

    Client and deliverables

--- paperwork/admin.py
+++ paperwork/admin.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Client, ClientInfoType, ClientInfo, Deadline, \
+    Deliverable, FinalDeadline, StepDeadline
+
 # Register your models here.
+
+admin.site.register(ClientInfoType)
+admin.site.register(Client)
+admin.site.register(ClientInfo)
+admin.site.register(Deadline)
+admin.site.register(Deliverable)
+admin.site.register(FinalDeadline)
+admin.site.register(StepDeadline)
