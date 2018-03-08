68dcb9f82e751fd5df2691be602c0b749caf079d
Mark Miller <725mrm@gmail.com>
2018-03-07 23:14:36 -0800

    Business days backend

--- paperwork/logic.py
+++ paperwork/logic.py
@@ -1,4 +1,6 @@
 #from dateutil.relativedelta import relativedelta
+from models.deliverables import DURATION_CHOICES
+import datetime
 
 def time_phrase(number, duration, relation):
 	# assume duration is days
@@ -7,3 +9,30 @@ def time_phrase(number, duration, relation):
 	elif relation == "before":
 		direction = -1
 	return int(number) * direction
+
+def calculate_calendar_day_offset(date, offset):
+	return date + datetime.timedelta(offset)
+
+def check_as_work_day(date):
+	return date.weekday() in [0, 1, 2, 3] # M T W R
+
+def calculate_business_day_offset(date, offset):
+	# in the future, call into an availability calendar.
+
+	# on a M T W R schedule, four business days is a week.
+	weeks = (offset - 1) / 4
+	days_from_week = weeks * 4
+	day_difference = offset - days_from_week
+	approximate_date = date + datetime.timedelta(days=weeks*7)
+	while day_difference > 0:
+		approximate_date = approximate_date + datetime.timedelta(days=1)
+		if check_as_work_day(approximate_date):
+			day_difference -= 1
+	return approximate_date
+
+
+def calculate_date(date, offset, duration):
+	if duration == DURATION_CHOICES.calendar_day:
+		return calculate_calendar_day_offset(date, offset)
+	elif duration == DURATION_CHOICES.business_day:
+		return calculate_business_day_offset(date, offset)
--- paperwork/tests.py
+++ paperwork/tests.py
@@ -3,4 +3,39 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from logic import calculate_business_day_offset
+from datetime import datetime
+
 # Create your tests here.
+
+class CalculateBusinessDayTestCase(TestCase):
+
+    def dates_and_offset(self, src_date_string, dst_date_string, offset):
+        src_date = datetime.strptime(src_date_string, '%y-%m-%d')
+        dst_date = datetime.strptime(dst_date_string, '%y-%m-%d')
+        test_dst_date = calculate_business_day_offset(src_date, offset)
+        self.assertEqual(test_dst_date, dst_date)
+
+    def test_weekday_ahead(self):
+        self.dates_and_offset('18-03-06', '18-03-07', 1) # Tuesday and Wednesday
+
+    def test_weekday_behind(self):
+        self.dates_and_offset('18-03-06', '18-03-05', -1) # Tuesday and Monday
+
+    def test_weekend_ahead(self):
+        self.dates_and_offset('18-03-10', '18-03-12', 1) # Saturday and Monday
+
+    def test_weekend_behind(self):
+        self.dates_and_offset('18-03-10', '18-03-07', -1) # Saturday and Wednesday
+
+    def test_weekday_ahead_multiple(self):
+        self.dates_and_offset('18-03-06', '18-03-21', 9) # Tuesday to Wednesday+2
+
+    def test_weekday_behind_multiple(self):
+        self.dates_and_offset('18-03-06', '18-02-22', -6) # Tuesday to Thursday-2
+
+    def test_weekend_ahead_multiple(self):
+        self.dates_and_offset('18-03-09', '18-03-21', 7) # Friday to Wednesday+2
+
+    def test_weekend_behind_multiple(self):
+        self.dates_and_offset('18-03-09', '18-02-22', -8) # Friday to Thursday-2
