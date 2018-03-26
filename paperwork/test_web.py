80c3ed0ecde68c8688efb6b9d2f0e8782f99d701
Mark Miller <725mrm@gmail.com>
2018-03-26 01:42:54 -0700

    minor refactor to pass style check

--- paperwork/test_web.py
+++ paperwork/test_web.py
@@ -1,17 +1,25 @@
+""" Tests for the web-particular things. Like clicking on buttons. """
+
 from django.test import LiveServerTestCase
 from selenium import webdriver
 
 class WebTestCase(LiveServerTestCase):
-
+    """ Functional tests for the way Jordan may use the website"""
     @classmethod
     def setUpClass(cls):
+        """ Create the webdriver """
         super(WebTestCase, cls).setUpClass()
         cls.browser = webdriver.Firefox()
 
     @classmethod
     def tearDownClass(cls):
+        """
+        Quit the webdriver so firefox windows
+        aren't littering my desktop
+        """
         cls.browser.quit()
         super(WebTestCase, cls).tearDownClass()
 
     def test_sanity(self):
+        """ a simple test case """
         self.browser.get(self.live_server_url)
