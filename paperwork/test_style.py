032babfaae53f13f5999b536e14904c2a16435ae
Mark Miller <725mrm@gmail.com>
2018-03-26 23:37:43 -0700

    separated style checks into test cases dynamically generated

--- paperwork/test_style.py
+++ paperwork/test_style.py
@@ -1,3 +1,8 @@
+"""
+Use pylint to check style. Test cases are dynamically generated.
+Isn't it cool?
+"""
+
 import os
 import sys
 import errno
@@ -7,6 +12,7 @@ from django.test import TestCase
 from pylint import epylint as lint
 
 def mkdir_p(path):
+    """ creates a directory if necessary """
     try:
         os.makedirs(path)
     except OSError as exc:  # Python >2.5
@@ -17,10 +23,11 @@ def mkdir_p(path):
 
 PYTHON_EXTENSION = '.py'
 PYLINT_OUT = "./paperwork/pylint_out"
-IGNORE_DIRS = ["./paperwork/migrations", PYLINT_OUT]
+ENABLE_DIRS = ["./paperwork"]
+DISABLE_DIRS = ["./paperwork/migrations", PYLINT_OUT]
 EXTRA_OPTIONS = " ".join([
     # need an empty string for that leading space.
-    "", 
+    "",
 
     # first, Django's subclassing of Models, etc. give lotsa errors.
     # Make Pylint smart about django.
@@ -29,37 +36,42 @@ EXTRA_OPTIONS = " ".join([
     "--max-line-length=80", # be rigorous in line length
 
     # test names are OK long because they aren't called in code.
-    "--method-rgx (([a-z_][a-z0-9_]{2,30})|" + 
+    "--method-rgx (([a-z_][a-z0-9_]{2,30})|" +
     "((test|assert)_[a-z_][a-z0-9_]{2,50}))$",
 ])
 
-class StyleTestCase(TestCase):
-    def test_run(self):
-        # keep a list of failing style files
+class StyleTestsMeta(type):
+    """
+    Metaclass that creates a test for each style check.
+    Mostly borrowed from
+    # pylint disable=max-line-length
+    https://chris-lamb.co.uk/posts/generating-dynamic-python-tests-using-metaclasses
+    # pylint enable=max-line-length
+    """
+    def __new__(mcs, name, bases, attrs):
+        """ tbh not sure when this function is called. """
+        # setup
         threshold_score = 8
-        failing_style_files = []
 
-        # make sure you can reach the linecount checker
-        sys.path.append('./paperwork/')
+        # walk through the files, making test classes for each file.
+        for enabled_dir in ENABLE_DIRS:
+            for root, _, files in os.walk(enabled_dir, topdown=True):
+                for fname in files:
+                    is_ignored = any([root.startswith(i) for i in DISABLE_DIRS])
+                    if not is_ignored and fname.endswith(PYTHON_EXTENSION):
+                        # make a test case for it!
+                        name_no_extension = fname[:-len(PYTHON_EXTENSION)]
+                        big_path = os.path.join(root, name_no_extension)
+                        underscored_path = big_path[2:].replace("/", "_")
+                        test_name = 'test_%s' % underscored_path
+                        attrs[test_name] = mcs.gen(root, fname, threshold_score)
 
-        mkdir_p(PYLINT_OUT)
-        for root, _, files in os.walk("./paperwork", topdown=True):
-            for name in files:
-                score = self.lint_file(root, name)
-                if score is not None and score < threshold_score:
-                    failing_style_files.append(os.path.join(root, name))
+        return super(StyleTestsMeta, mcs).__new__(mcs, name, bases, attrs)
 
-        if failing_style_files:
-            self.assertEqual(
-                len(failing_style_files),
-                0,
-                "{l} style checks failed: {cases}".format(
-                    l=len(failing_style_files),
-                    cases=failing_style_files))
-
-    def lint_file(self, root, name):
-        is_ignored = any([root.startswith(i) for i in IGNORE_DIRS])
-        if not is_ignored and name.endswith(PYTHON_EXTENSION):
+    @classmethod
+    def gen(mcs, root, name, threshold_score):
+        # Return a testcase that checks the style of file with path.
+        def generated_function(self):
             mkdir_p(os.path.join(PYLINT_OUT, root))
             target_filename = os.path.join(root, name)
             (pylint_stdout, _) = lint.py_run(
@@ -75,7 +87,18 @@ class StyleTestCase(TestCase):
             regex = r"Your code has been rated at (-?[\d\.]+)/10"
             matches = re.findall(regex, full_output)
             self.assertLessEqual(len(matches), 1)
-            if len(matches) == 1:
-                return float(matches[0])
-        return None
-                
+            # sometimes, no matches are made if pylint is parsing an empty file
+            if matches:
+                score = float(matches[0])
+                self.assertGreaterEqual(score, threshold_score)
+
+        return generated_function
+
+class StyleTests(TestCase):
+    __metaclass__ = StyleTestsMeta
+
+    @classmethod
+    def setUpClass(cls):
+        super(StyleTests, cls).setUpClass()
+        # make sure you can reach the linecount checker
+        sys.path.append('./paperwork/')
